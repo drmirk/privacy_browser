7a236542bcc2f9fadbb9e520c778aeab3e222df5
Ibrahim Rasel <md.ibrahim.rasel@gmail.com>
2019-04-08 09:45:03 +0600

    Clear cache and clear sesssion

--- lib/src/body.dart
+++ lib/src/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_inappbrowser/flutter_inappbrowser.dart';
 import 'package:validators/validators.dart';
+import 'dart:async';
 
 class Home extends StatefulWidget {
   @override
@@ -9,11 +10,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double progress = 0;
   var url = 'https://www.google.com';
+  Icon _searchIcon = Icon(Icons.search);
+  Widget _appBarTitle = Text('Google');
 
   InAppWebViewController webView;
-
-  double progress = 0;
 
   openUrl(value) {
     if (isURL(value)) {
@@ -22,26 +24,30 @@ class _HomeState extends State<Home> {
       value = "http://www.google.com/search?q=$value";
       webView.loadUrl(value);
     }
+    _searchPressed();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: addressBar(),
+      appBar: addressBar2(),
       body: InAppWebView(
         initialUrl: url,
         initialHeaders: {},
-        initialOptions: {},
+        initialOptions: {
+          "clearCache": true,
+          "clearSessionCache": true,
+        },
         onWebViewCreated: (InAppWebViewController controller) {
           webView = controller;
         },
-        onLoadStart: (InAppWebViewController controller, String url) {
+        onLoadStart: (controller, url) {
           print("started $url");
           setState(() {
             this.url = url;
           });
         },
-        onProgressChanged: (InAppWebViewController controller, int progress) {
+        onProgressChanged: (controller, progress) {
           setState(
             () {
               this.progress = progress / 100;
@@ -52,13 +58,42 @@ class _HomeState extends State<Home> {
     );
   }
 
+  void _searchPressed() {
+    setState(() {
+      if (this._searchIcon.icon == Icons.search) {
+        this._searchIcon = new Icon(Icons.close);
+        this._appBarTitle = TextField(
+          decoration: InputDecoration(
+            isDense: true,
+          ),
+          keyboardType: TextInputType.url,
+          onSubmitted: openUrl,
+        );
+      } else {
+        this._searchIcon = Icon(Icons.search);
+        this._appBarTitle = Text('Search Example');
+      }
+    });
+  }
+
+  Widget addressBar2() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: _appBarTitle,
+      actions: <Widget>[
+        IconButton(
+          icon: _searchIcon,
+          onPressed: _searchPressed,
+        ),
+      ],
+    );
+  }
+
   Widget addressBar() {
     return AppBar(
       backgroundColor: Colors.white,
       title: TextField(
         decoration: InputDecoration(
-          prefixText: 'https://',
-          hintText: "${url.substring(8)}",
           isDense: true,
         ),
         keyboardType: TextInputType.url,
